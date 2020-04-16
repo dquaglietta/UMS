4bae01cf53d1869875671690946e1680b079b8fd
dquaglietta@minsait.com <dquaglietta@minsait.com>
2020-04-16 16:53:15 +0200

    primo commit sito angular ums

---
+++ ums/src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'tr[app-user]',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  //inputs: ['user:user-data']
+})
+export class UserComponent implements OnInit {
+  @Input('user-data') user;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
